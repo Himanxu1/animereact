72c0bffb629ee12f20052a8ce80bd370b6d9d8e1
Himanshu <technophilic000@gmail.com>
2022-05-04 07:53:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {motion} from 'framer-motion';
+import {useState} from 'react';
+
 
 function App() {
+  const [isopen,setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div className='card' onClick={() => setIsOpen(!isopen)} layout>
+        <motion.h2>Framer Motion</motion.h2>
+        {isopen &&  <motion.div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi cumque doloremque. Culpa nam ex ipsam, iure laborum reprehenderit laudantium.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quidem!</p>
+        </motion.div>}
+      </motion.div>
+     
     </div>
   );
 }
